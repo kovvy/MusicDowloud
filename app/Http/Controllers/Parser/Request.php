3a4d3085f8cc1e0f4fc7e8e8eb458ea49fcb6133
Igor <kovalevgr@gmail/com>
2017-01-25 02:51:23 +0200

    Получаем HTML

--- app/Http/Controllers/Parser/Request.php
+++ app/Http/Controllers/Parser/Request.php
@@ -8,4 +8,18 @@ class RequestController
 {
     const BASE_URL = 'http://mp3.cc/';
 
+    private $headers = [];
+
+    protected function getDocument(array $options = NULL, $body = NULL)
+    {
+        $document = hQuery::fromUrl(self::BASE_URL, $this->headers);
+
+        if (!$document)
+        {
+            abort(404);
+        }
+
+        return $document;
+
+    }
 }
