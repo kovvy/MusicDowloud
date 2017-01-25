82f3a9918fdec1a92bb6946fca14e42cc4be382b
Igor <kovalevgr@gmail/com>
2017-01-25 04:40:15 +0200

    set param

--- app/Http/Controllers/Parser/Request.php
+++ app/Http/Controllers/Parser/Request.php
@@ -1,18 +1,30 @@
 <?php
 
-namespace app\Http\Controllers\Parser;
+namespace App\Http\Controllers\Parser;
 
 use hQuery;
 
-class RequestController
+class Request
 {
     const BASE_URL = 'http://mp3.cc/';
 
-    private $headers = [];
+    private $url = self::BASE_URL;
 
-    protected function getDocument(array $options = NULL, $body = NULL)
+    private $headers;
+
+    private $body;
+
+    private $options;
+
+    protected function getDocument()
     {
-        $document = hQuery::fromUrl(self::BASE_URL, $this->headers);
+        try {
+
+            $document = hQuery::fromUrl($this->url, $this->headers, $this->body, $this->options);
+
+        } catch (\Exception $e) {
+            dd($e);
+        }
 
         if (!$document)
         {
@@ -21,5 +33,31 @@ class RequestController
 
         return $document;
 
+    }
+
+    protected function setUrl($url)
+    {
+        $this->url = self::BASE_URL . $url;
+    }
+
+    protected function setHeaders(array $headers)
+    {
+        foreach ($headers as $name => $value) {
+            $this->headers[$name] = $value;
+        }
+    }
+
+    protected function setBody($body)
+    {
+        foreach ($body as $name => $value) {
+            $this->$body[$name] = $value;
+        }
+    }
+
+    protected function setOptions(array $options)
+    {
+        foreach ($options as $name => $value) {
+            $this->$options[$name] = $value;
+        }
     }
 }
