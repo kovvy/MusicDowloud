7cf7e0e0672a96ce2154c447af430f0fc93d8cc6
Igor <kovalevgr@gmail/com>
2017-02-19 23:06:19 +0200

    add Proxy CURL

--- app/Http/Controllers/Parser/Request.php
+++ app/Http/Controllers/Parser/Request.php
@@ -26,39 +26,34 @@ class Request
 
     protected function getDocument($method = 'GET')
     {
+        set_time_limit(180);
 
+        $proxy = '178.54.44.24:8080';
         $ch = curl_init();
-
         curl_setopt($ch, CURLOPT_URL, $this->url);
-        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_PROXY, $proxy);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+//        curl_setopt($ch, CURLOPT_TIMEOUT, 200);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+//        curl_setopt($ch, CURLOPT_MAXREDIRS, 2);
+        curl_setopt($ch, CURLOPT_USERAGENT,
+            "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.7) Gecko/20070914 Firefox/2.0.0.7");
+        curl_setopt($ch, CURLOPT_COOKIEJAR, "cookies.txt");
+        curl_setopt($ch, CURLOPT_COOKIEFILE, "cookies.txt");
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_ENCODING, "gzip");
 
-        if ($method == 'POST' AND !empty($this->body))
-        {
-            curl_setopt($ch, CURLOPT_POST, true);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $this->body);
-        }
-
-        $content = curl_exec($ch);
-        $err     = curl_errno($ch);
-        $errmsg  = curl_error($ch);
-        $header  = curl_getinfo($ch);
-
+        $data = curl_exec($ch);
         curl_close($ch);
 
-        if ($err !== 0)
+        if (!$data)
         {
-            Log::error($errmsg);
-            abort(503);
-        } elseif (!$content)
-        {
-            Log::error('Error cURL. Content is empty!');
+            Log::error('Error query');
             abort(503);
         }
 
-        return $content;
+        return $data;
 
     }
 }
